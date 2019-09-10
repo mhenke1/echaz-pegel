9a49ad217f38a20e08462c2863b3a6a98b8ae6e3
Martin Henke <martin.henke@web.de>
2019-09-10 23:59:25 +0200

    animated icons depending on water level

--- app.js
+++ app.js
@@ -9,19 +9,51 @@ const pegelCache = new NodeCache({stdTTL: 60, checkperiod: 180});
 
 const requestString = 'https://www.hvz.baden-wuerttemberg.de/js/hvz-data-peg-db.js';
 
+function inrange(value, min, max) {
+  if (value >= min && value <= max){
+    return true;
+  }
+  return false;
+}
+
+function selectIcon(depth) {
+  let depthInt = parseInt(depth);
+  if (inrange(depth,0,30)) {
+    return "24114"
+  }
+  if (inrange(depth,31,60)) {
+    return "24115"
+  }
+  if (inrange(depth,61,100)) {
+    return "24116"
+  }
+  if (inrange(depth,101,130)) {
+    return "24117"
+  }
+  if (inrange(depth,131,150)) {
+    return "24118"
+  }
+  if (inrange(depth,151,200)) {
+    return "24119"
+  }
+  if (inrange(depth,201,400)) {
+    return "24120"
+  }
+}
+
 function getPegel() {
   return new Promise((resolve, reject) => {
     request(requestString)
       .then((response) => {
         const regex = /Echaz\',.*?,'(.*?)'/gm;
-        var match = regex.exec(response.body);
+        let match = regex.exec(response.body);
         let depth = match[1];
         resolve(
           {
             'frames': [
               {
                 'text': 'Echaz ' + depth,
-                'icon': 'i19772',
+                'icon': 'i'+ selectIcon(depth),
               },
             ],
           });
